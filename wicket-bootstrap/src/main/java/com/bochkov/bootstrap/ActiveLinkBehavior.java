13d0ee8f26d02e0559bb28e2775b7f167dd582d9
bochkov <bochkov2006@yandex.ru>
2019-09-13 17:18:36 +1200

    project created

--- wicket-bootstrap/src/main/java/com/bochkov/bootstrap/ActiveLinkBehavior.java
+++ wicket-bootstrap/src/main/java/com/bochkov/bootstrap/ActiveLinkBehavior.java
@@ -4,25 +4,41 @@ import org.apache.wicket.ClassAttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.danekja.java.util.function.serializable.SerializableFunction;
 
 import java.util.Set;
 
-public class ActiveLinkBehavior extends Behavior {
+public abstract class ActiveLinkBehavior<C extends Component> extends Behavior {
+    public static ActiveLinkBehavior<BookmarkablePageLink> of(BookmarkablePageLink link) {
+        return new ActiveLinkBehavior<BookmarkablePageLink>() {
+            @Override
+            public boolean isActive(BookmarkablePageLink link) {
+                return link.getPageClass().isAssignableFrom(link.getPage().getPageClass());
+            }
+        };
+    }
+
+    public static <C extends Component> ActiveLinkBehavior<C> of(C component, final SerializableFunction<C, Boolean> predicate) {
+        return new ActiveLinkBehavior<C>() {
+            public boolean isActive(C component) {
+                return predicate.apply(component);
+            }
+        };
+    }
+
+
     @Override
     public void bind(final Component component) {
         super.bind(component);
-        if (!(component instanceof BookmarkablePageLink)) {
-            throw new IllegalArgumentException(String.format("ActiveLinkBehavior is for BookmarkablePageLink.class not for %s", component.getClass()));
-        }
-        final BookmarkablePageLink link = (BookmarkablePageLink) component;
         component.add(new ClassAttributeModifier() {
             @Override
             protected Set<String> update(Set<String> oldClasses) {
-                if (link.getPageClass().isAssignableFrom(link.getPage().getPageClass())) {
+                if (isActive((C) component))
                     oldClasses.add("active");
-                }
                 return oldClasses;
             }
         });
     }
+
+    public abstract boolean isActive(C component);
 }
