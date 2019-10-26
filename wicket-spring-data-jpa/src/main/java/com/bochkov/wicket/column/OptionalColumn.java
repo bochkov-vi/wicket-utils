2e68ed779976bad25378836b565fc902ae800571
bochkov <bochkov2006@yandex.ru>
2019-10-26 13:05:51 +1200

    submit javadoc

--- wicket-spring-data-jpa/src/main/java/com/bochkov/wicket/column/OptionalColumn.java
+++ wicket-spring-data-jpa/src/main/java/com/bochkov/wicket/column/OptionalColumn.java
@@ -10,6 +10,15 @@ public class OptionalColumn<T, S> extends LambdaColumn<T, S> {
 
     SerializableFunction<Optional<T>, Optional> function;
 
+    public static <T> OptionalColumn<T, String> of(IModel<String> displayModel, SerializableFunction<Optional<T>, Optional> function) {
+        return new OptionalColumn<>(displayModel, function);
+    }
+
+    public static <T, S> OptionalColumn<T, S> of(IModel<String> displayModel, S sortProperty, SerializableFunction<Optional<T>, Optional> function) {
+        return new OptionalColumn<T, S>(displayModel, sortProperty, function);
+    }
+
+
     public OptionalColumn(IModel<String> displayModel, SerializableFunction<Optional<T>, Optional> function) {
         super(displayModel, (SerializableFunction<T, Object>) t -> function.apply(Optional.ofNullable(t)).orElse(null));
         this.function = function;
