e1ac4d1e65e09aaef21b6462f9491dd3eb7bdf02
bochkov-vi <bochkov2006@yandex.ru>
2019-11-24 09:55:26 +1200

    any fix

--- wicket-spring-data-jpa/src/main/java/com/bochkov/wicket/component/select2/data/PageableStringChoiceProvider.java
+++ wicket-spring-data-jpa/src/main/java/com/bochkov/wicket/component/select2/data/PageableStringChoiceProvider.java
@@ -1,12 +1,24 @@
 package com.bochkov.wicket.component.select2.data;
 
-import java.util.Collection;
+import org.danekja.java.util.function.serializable.SerializableBiFunction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 /**
  * The type Pageable string choice provider.
  */
 public abstract class PageableStringChoiceProvider extends PageableChoiceProvider<String> {
 
+
+    public PageableStringChoiceProvider of(SerializableBiFunction<String, Pageable, Page<String>> finder) {
+        PageableStringChoiceProvider provider = new PageableStringChoiceProvider() {
+            @Override
+            public Page<String> findByMask(String term, Pageable pageRequest) {
+                return finder.apply(term, pageRequest);
+            }
+        };
+        return provider;
+    }
 
     @Override
     public String getDisplayValue(String object) {
