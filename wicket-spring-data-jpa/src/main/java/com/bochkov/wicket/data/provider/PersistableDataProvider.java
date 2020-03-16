88c2dd0fa23b31f1cc5b39a017c9474d42d58347
bochkov-vi <bochkov2006@yandex.ru>
2020-03-17 09:12:15 +1200

    split interfaces MaskableChoiceProvide and EntityChoiceProvider

--- wicket-spring-data-jpa/src/main/java/com/bochkov/wicket/data/provider/PersistableDataProvider.java
+++ wicket-spring-data-jpa/src/main/java/com/bochkov/wicket/data/provider/PersistableDataProvider.java
@@ -1,5 +1,6 @@
-package com.bochkov.wicket.data.model;
+package com.bochkov.wicket.data.provider;
 
+import com.bochkov.wicket.data.model.PersistableModel;
 import org.apache.wicket.extensions.markup.html.repeater.util.SingleSortState;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
