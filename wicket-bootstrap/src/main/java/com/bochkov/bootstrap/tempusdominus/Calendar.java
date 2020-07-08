00e644f5da84fee4da543388b8ad1d4681399f38
bochkov-vi <bochkov2006@yandex.ru>
2020-07-09 06:02:12 +1200

    add tempus dominus components

--- wicket-bootstrap/src/main/java/com/bochkov/bootstrap/tempusdominus/Calendar.java
+++ wicket-bootstrap/src/main/java/com/bochkov/bootstrap/tempusdominus/Calendar.java
@@ -5,19 +5,23 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ClassAttributeModifier;
+import org.apache.wicket.extensions.markup.html.form.datetime.LocalDateTextField;
 import org.apache.wicket.markup.html.form.AbstractTextComponent;
 import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.FormComponentPanel;
 import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.danekja.java.util.function.serializable.SerializableFunction;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Set;
 
 @Getter
 @Setter
 @Accessors(chain = true)
-public abstract class Calendar<T extends Serializable> extends GenericPanel<T> implements AbstractTextComponent.ITextFormatProvider, ITempusdominus {
+public abstract class Calendar<T extends Serializable> extends FormComponentPanel<T> implements AbstractTextComponent.ITextFormatProvider, ITempusdominus {
 
     TextField<T> textField = createInput("input-text");
 
@@ -31,6 +35,19 @@ public abstract class Calendar<T extends Serializable> extends GenericPanel<T> i
 
     public Calendar(String id, IModel<T> model) {
         super(id, model);
+    }
+
+    public static <T extends Serializable, F extends TextField<T> & AbstractTextComponent.ITextFormatProvider> Calendar<T> of(String compId, IModel<T> model, SerializableFunction<String, F> inputCreator) {
+        return new Calendar<T>(compId, model) {
+            @Override
+            public F createInput(String id) {
+                return inputCreator.apply(id);
+            }
+        };
+    }
+
+    public static Calendar<LocalDate> localDate(String compId, IModel<LocalDate> model, String pattern) {
+        return of(compId, model, (id) -> new LocalDateTextField(id, pattern));
     }
 
     @Override
@@ -50,8 +67,19 @@ public abstract class Calendar<T extends Serializable> extends GenericPanel<T> i
         add(button);
         button.add(new AttributeModifier("data-target", String.format("#%s", getMarkupId())));
         textField.add(new AttributeModifier("data-target", String.format("#%s", getMarkupId())));
-        textField.setModel(getModel());
+        textField.setModel(Model.of());
         add(behavior.setDateOnly(true));
+    }
+
+    @Override
+    protected void onBeforeRender() {
+        super.onBeforeRender();
+        textField.setModelObject(getModelObject());
+    }
+
+    @Override
+    public void convertInput() {
+        setConvertedInput(textField.getConvertedInput());
     }
 
     public abstract <F extends TextField<T> & AbstractTextComponent.ITextFormatProvider> F createInput(String id);
