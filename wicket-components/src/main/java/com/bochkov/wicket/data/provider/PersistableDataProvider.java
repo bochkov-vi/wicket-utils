8488003d483ee77f542ea7215736be0102a74280
bochkov-vi <bochkov2006@yandex.ru>
2021-03-03 10:16:55 +1200

    second commit

--- wicket-components/src/main/java/com/bochkov/wicket/data/provider/PersistableDataProvider.java
+++ wicket-components/src/main/java/com/bochkov/wicket/data/provider/PersistableDataProvider.java
@@ -1,6 +1,6 @@
 package com.bochkov.wicket.data.provider;
 
-import com.bochkov.wicket.data.model.PersistableModel;
+import com.bochkov.wicket.jpa.model.PersistableModel;
 import org.apache.wicket.extensions.markup.html.repeater.util.SingleSortState;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
